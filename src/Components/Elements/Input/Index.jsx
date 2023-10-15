7189f11f953c0ca450aa77e18fd27fbf489d11e3
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-15 19:20:27 +0800

    Feat: create element FormInput

---
+++ src/Components/Elements/Input/Index.jsx
@@ -0,0 +1,15 @@
+import Input from "./Input";
+import Label from "./Label";
+
+const FormInput = (props) => {
+   const { label, htmlFor, type, placeholder } = props;
+
+   return (
+      <div className="mb-6">
+         <Label htmlFor={htmlFor}>{label}</Label>
+         <Input type={type} id={htmlFor} placeholder={placeholder} />
+      </div>
+   );
+};
+
+export default FormInput;
