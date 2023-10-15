59bcaaf7fe7d779ee11205cc86e9c33e7021d99f
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-15 22:39:39 +0800

    Feat: added link to each other (login and register)

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,10 +1,17 @@
 import AuthLayouts from "../Components/Layouts/AuthLayouts";
 import FormLogin from "../Components/Fragments/FormLogin";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
    return (
       <AuthLayouts title="Login">
          <FormLogin />
+         <p className="text-center text-gray-300 mb-2">
+            Don't have an account?{" "}
+            <Link to="/register">
+               <span className="text-sky-500">Sign up</span>
+            </Link>
+         </p>
       </AuthLayouts>
    );
 };
