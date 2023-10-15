332d2a3e298c7b89ac92def42c66b2f2c96b8865
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-15 14:49:28 +0800

    Feat: learn about component and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+// Component Button
+const Button = (props) => {
+   // destructuring obj, dan set default value pada setiap property.
+   const { children = "....", variant = "bg-black" } = props;
+   return (
+      <button className={`px-6 h-10 font-semibold rounded-md ${variant}`}>
+         {children}
+      </button>
+   );
+};
+
 function App() {
    return (
       <>
-         <h1 className="flex justify-center bg-slate-700 min-h-screen items-center text-white">
-            Hello World, This is my first react app
-         </h1>
+         <div className="flex flex-col gap-y-3 justify-center bg-slate-700 min-h-screen items-center">
+            <h1 className="text-white">
+               Hello World, This is my first react app
+            </h1>
+            <div className="flex gap-x-2">
+               <Button variant="bg-sky-700 text-white">Login</Button>
+               <Button variant="bg-green-700 text-white">Register</Button>
+               <Button variant="bg-pink-700 text-white">Logout</Button>
+            </div>
+         </div>
       </>
    );
 }
