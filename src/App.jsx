409794041b39e3075e7393ea0dbc8d24699c5b5d
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-15 19:19:46 +0800

    Feat: added LoginPage and RegisterPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import Button from "./Components/Elements/Button/Index";
+import LoginPage from "./Pages/login";
+import RegisterPage from "./Pages/register";
 
 function App() {
    return (
       <>
-         <div className="flex flex-col gap-y-3 justify-center bg-slate-700 min-h-screen items-center">
-            <h1 className="text-white">
-               Hello World, This is my first react app
-            </h1>
-            <div className="flex gap-x-2">
-               <Button variant="bg-sky-700 text-white">Login</Button>
-               <Button variant="bg-green-700 text-white">Register</Button>
-               <Button variant="bg-pink-700 text-white">Logout</Button>
-               <Button></Button> {/* Display default value */}
-            </div>
+         <div className="bg-slate-800 min-h-screen w-full">
+            {/* <LoginPage /> */}
+            <RegisterPage />
          </div>
       </>
    );
