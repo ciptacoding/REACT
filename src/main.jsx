1611332bd23e547d031af2cf60484978f07fad5a
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-15 22:37:33 +0800

    Feat: configuration react router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./Pages/login";
+import RegisterPage from "./Pages/register";
+import ErrorPage from "./Pages/error-page";
+import IndexPage from "./Pages";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <IndexPage />,
+      errorElement: <ErrorPage />,
+   },
+   {
+      path: "/login",
+      element: <LoginPage />,
+      errorElement: <ErrorPage />,
+   },
+   {
+      path: "/register",
+      element: <RegisterPage />,
+      errorElement: <ErrorPage />,
+   },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
    </React.StrictMode>
 );
