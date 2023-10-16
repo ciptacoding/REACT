a0c036b07555a8ad3f169699570a139c718326e1
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-16 23:03:43 +0800

    Feat: create conditional rendering

--- src/Components/Layouts/AuthLayouts.jsx
+++ src/Components/Layouts/AuthLayouts.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 const AuthLayouts = (props) => {
-   const { title, children } = props;
+   const { title, children, type } = props;
    return (
       <div className="flex min-h-screen w-full justify-center items-center">
          <div className="max-w-md bg-slate-700 px-8 py-4 rounded-md shadow-slate-900 shadow-lg">
@@ -10,6 +12,26 @@ const AuthLayouts = (props) => {
                Welcome back!, please enter your details below 👇🏼
             </p>
             {children}
+
+            {/* contidional rendering navigation auth links */}
+            <p className="text-center text-gray-300 mb-2">
+               {type === "login"
+                  ? "Don't have an account? "
+                  : "Already have an account? "}
+
+               {type === "login" && (
+                  <Link to="/login">
+                     <span className="text-sky-500">Sign in</span>
+                  </Link>
+               )}
+
+               {type === "register" && (
+                  <Link to="/register">
+                     <span className="text-sky-500">Sign up</span>
+                  </Link>
+               )}
+               {/* navigation rendering auth links */}
+            </p>
          </div>
       </div>
    );
