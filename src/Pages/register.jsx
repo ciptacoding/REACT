cd462e652e4659a4cec28df463c6fde51f60e657
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-16 23:04:30 +0800

    feat: Refactoring code

--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -4,14 +4,8 @@ import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
    return (
-      <AuthLayouts title="Register">
+      <AuthLayouts title="Register" type="register">
          <FormRegister />
-         <p className="text-center text-gray-300 mb-2">
-            Have an account?{" "}
-            <Link to="/login">
-               <span className="text-sky-500">Sign in</span>
-            </Link>
-         </p>
       </AuthLayouts>
    );
 };
