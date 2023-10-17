e9dc231dfb29ff7095d40a989cd10f18814f4042
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-17 16:42:07 +0800

    Feat: create products pages

---
+++ src/Pages/products.jsx
@@ -0,0 +1,12 @@
+import CardProduct from "../Components/Fragments/CardProduct";
+import MainLayouts from "../Components/Layouts/MainLayouts";
+
+const ProductsPage = () => {
+   return (
+      <MainLayouts title="Products">
+         <CardProduct></CardProduct>
+      </MainLayouts>
+   );
+};
+
+export default ProductsPage;
