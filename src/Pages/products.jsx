3b70dee45ac21480c85ecfa9fac65060b8b43cd3
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-17 18:22:05 +0800

    Feat: added props params in products

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -4,7 +4,16 @@ import MainLayouts from "../Components/Layouts/MainLayouts";
 const ProductsPage = () => {
    return (
       <MainLayouts title="Products">
-         <CardProduct></CardProduct>
+         <CardProduct>
+            <CardProduct.Header image="./images/shoes2.jpg" />
+            <CardProduct.Body
+               title="Sepatu paling keren sedunia"
+               location="Denpasar"
+               price="950.000"
+               sold="500"
+            />
+            <CardProduct.Footer />
+         </CardProduct>
       </MainLayouts>
    );
 };
