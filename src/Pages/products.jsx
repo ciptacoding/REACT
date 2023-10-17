5322fd990266d44bb4a6ff6ee4dc52fd56cfbc46
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-17 20:13:16 +0800

    Feat: added static data for card

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,31 +1,65 @@
 import CardProduct from "../Components/Fragments/CardProduct";
 import MainLayouts from "../Components/Layouts/MainLayouts";
 
+const products = [
+   {
+      id: 1,
+      image: "./images/shoes2.jpg",
+      name: "Sepatu keren banget 2023",
+      location: "Jawa Barat",
+      price: "1.500.000",
+      sold: 700,
+   },
+   {
+      id: 2,
+      image: "./images/shoes2.jpg",
+      name: "Sepatu keren sedunia",
+      location: "Denpasar",
+      price: "1.200.000",
+      sold: 354,
+   },
+   {
+      id: 3,
+      image: "./images/shoes2.jpg",
+      name: "Sepatu keren sedunia",
+      location: "Denpasar",
+      price: "1.200.000",
+      sold: 354,
+   },
+   {
+      id: 4,
+      image: "./images/shoes2.jpg",
+      name: "Sepatu keren sedunia",
+      location: "Denpasar",
+      price: "1.200.000",
+      sold: 354,
+   },
+   {
+      id: 5,
+      image: "./images/shoes2.jpg",
+      name: "Sepatu keren sedunia",
+      location: "Denpasar",
+      price: "1.200.000",
+      sold: 354,
+   },
+];
+
 const ProductsPage = () => {
    return (
       <MainLayouts title="Products">
          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center gap-4">
-            <CardProduct>
-               <CardProduct.Header image="./images/shoes2.jpg" />
-               <CardProduct.Body
-                  title="Sepatu paling keren sedunia"
-                  location="Denpasar"
-                  price="950.000"
-                  sold="500"
-               />
-               <CardProduct.Footer />
-            </CardProduct>
-
-            <CardProduct>
-               <CardProduct.Header image="./images/shoes2.jpg" />
-               <CardProduct.Body
-                  title="Sepatu paling keren sedunia"
-                  location="Denpasar"
-                  price="950.000"
-                  sold="500"
-               />
-               <CardProduct.Footer />
-            </CardProduct>
+            {products.map((product) => (
+               <CardProduct key={product.id}>
+                  <CardProduct.Header image={product.image} />
+                  <CardProduct.Body
+                     name={product.name}
+                     location={product.location}
+                     price={product.price}
+                     sold={product.sold}
+                  />
+                  <CardProduct.Footer />
+               </CardProduct>
+            ))}
          </div>
       </MainLayouts>
    );
