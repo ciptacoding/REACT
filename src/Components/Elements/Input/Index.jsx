c614d3922c6a82b65d61d783182a3892f8898f89
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-18 22:54:37 +0800

    Feat: added props name

--- src/Components/Elements/Input/Index.jsx
+++ src/Components/Elements/Input/Index.jsx
@@ -3,12 +3,17 @@ import Input from "./Input";
 import Label from "./Label";
 
 const FormInput = (props) => {
-   const { label, htmlFor, type, placeholder } = props;
+   const { label, htmlFor, type, placeholder, name } = props;
 
    return (
       <div className="mb-6">
          <Label htmlFor={htmlFor}>{label}</Label>
-         <Input type={type} id={htmlFor} placeholder={placeholder} />
+         <Input
+            type={type}
+            id={htmlFor}
+            placeholder={placeholder}
+            name={name}
+         />
       </div>
    );
 };
