b026c8e4b819be94721e14b932c5cfec2c77a01b
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-18 23:40:08 +0800

    Feat: create handleLogout in pages products

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,3 +1,4 @@
+import Button from "../Components/Elements/Button/Index";
 import CardProduct from "../Components/Fragments/CardProduct";
 import MainLayouts from "../Components/Layouts/MainLayouts";
 
@@ -44,24 +45,40 @@ const products = [
    },
 ];
 
+const email = localStorage.getItem("email");
+
 const ProductsPage = () => {
+   const handleLogout = () => {
+      localStorage.removeItem("email");
+      localStorage.removeItem("password");
+      window.location.href = "/login";
+   };
+
    return (
-      <MainLayouts title="Products">
-         <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center gap-4">
-            {products.map((product) => (
-               <CardProduct key={product.id}>
-                  <CardProduct.Header image={product.image} />
-                  <CardProduct.Body
-                     name={product.name}
-                     location={product.location}
-                     price={product.price}
-                     sold={product.sold}
-                  />
-                  <CardProduct.Footer />
-               </CardProduct>
-            ))}
+      <>
+         <div className="flex justify-end pt-5 pr-10 items-center gap-5">
+            <span className="text-gray-300 font-semibold">{email}</span>
+            <Button variant="bg-pink-600 text-gray-300" onClick={handleLogout}>
+               Logout
+            </Button>
          </div>
-      </MainLayouts>
+         <MainLayouts title="Products">
+            <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center gap-4">
+               {products.map((product) => (
+                  <CardProduct key={product.id}>
+                     <CardProduct.Header image={product.image} />
+                     <CardProduct.Body
+                        name={product.name}
+                        location={product.location}
+                        price={product.price}
+                        sold={product.sold}
+                     />
+                     <CardProduct.Footer />
+                  </CardProduct>
+               ))}
+            </div>
+         </MainLayouts>
+      </>
    );
 };
 
