87e8f5888228ac60fdcfb4d58ff011427547fb04
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-18 22:54:55 +0800

    Feat: create name props

--- src/Components/Elements/Input/Input.jsx
+++ src/Components/Elements/Input/Input.jsx
@@ -1,12 +1,13 @@
 // Input.jsx
 const Input = (props) => {
-   const { type, placeholder, id } = props;
+   const { type, placeholder, id, name } = props;
    return (
       <input
          type={type}
          className="w-full bg-slate-900 py-2 px-4 text-gray-400 rounded-md placeholder:opacity-40"
          placeholder={placeholder}
          id={id}
+         name={name}
       />
    );
 };
