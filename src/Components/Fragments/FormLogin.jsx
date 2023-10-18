dd8882b86add0134a1584382dadb8d223188a649
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-18 23:39:34 +0800

    Feat: create event handler in FormLogin

--- src/Components/Fragments/FormLogin.jsx
+++ src/Components/Fragments/FormLogin.jsx
@@ -2,8 +2,15 @@ import FormInput from "../Elements/Input/Index";
 import Button from "../Elements/Button/Index";
 
 const FormLogin = () => {
+   const handleLogin = (event) => {
+      event.preventDefault();
+      localStorage.setItem("email", event.target.email.value);
+      localStorage.setItem("password", event.target.password.value);
+      window.location.href = "/products";
+   };
+
    return (
-      <form>
+      <form onSubmit={handleLogin} method="POST">
          <FormInput
             label="Email"
             htmlFor="email"
@@ -20,7 +27,10 @@ const FormLogin = () => {
             name="password"
          />
 
-         <Button variant="bg-sky-700 text-gray-300 w-full hover:bg-sky-800 hover:text-gray-400 mb-4">
+         <Button
+            variant="bg-sky-700 text-gray-300 w-full hover:bg-sky-800 hover:text-gray-400 mb-4"
+            type="submit"
+         >
             Sign In
          </Button>
       </form>
