3b341738c8a5c654abc4c3d524b4ca01d70dab9d
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-18 23:39:11 +0800

    Feat: create new props to element Button

--- src/Components/Elements/Button/Index.jsx
+++ src/Components/Elements/Button/Index.jsx
@@ -2,9 +2,18 @@
 const Button = (props) => {
    // destructuring obj, dan set default value pada setiap property.
    // property children menangkap semua element yang ada didalam parentnya.
-   const { children = "....", variant = "bg-gray-400" } = props;
+   const {
+      children = "....",
+      variant = "bg-gray-400",
+      type = "button",
+      onClick = () => {},
+   } = props;
    return (
-      <button className={`px-6 py-2 font-semibold rounded-md ${variant}`}>
+      <button
+         className={`px-6 py-2 font-semibold rounded-md ${variant}`}
+         type={type}
+         onClick={onClick}
+      >
          {children}
       </button>
    );
