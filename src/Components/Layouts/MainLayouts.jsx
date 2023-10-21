f79e8fbb602f02b5716c5ad63e34cc51576d3d53
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-21 21:40:10 +0800

    Feat: redesign UI in MainLayouts

--- src/Components/Layouts/MainLayouts.jsx
+++ src/Components/Layouts/MainLayouts.jsx
@@ -1,14 +1,10 @@
 import { Link } from "react-router-dom";
 
 const MainLayouts = (props) => {
-   const { title, children } = props;
+   const { children } = props;
 
    return (
-      <div className="min-h-screen max-w-7xl mx-auto">
-         <h1 className="text-3xl font-bold text-gray-300 text-center mb-5 pt-5">
-            {title}
-         </h1>
-
+      <div className="min-h-screen px-8 xl:px-4 max-w-7xl mx-auto">
          {children}
       </div>
    );
