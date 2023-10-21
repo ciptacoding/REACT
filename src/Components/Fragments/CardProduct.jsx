8d408414d3ae7a69f9f5626d9946cddeef94139b
Cipta Dwipajaya <ciptacoding@gmail.com>
2023-10-21 21:39:43 +0800

    Feat: added local currency and button add to cart

--- src/Components/Fragments/CardProduct.jsx
+++ src/Components/Fragments/CardProduct.jsx
@@ -31,17 +31,28 @@ const CardBody = (props) => {
             <p className="text-xs text-gray-400 pt-1">{location}</p>
          </div>
          <div className="px-3 pb-2 flex justify-between items-center gap-2">
-            <span className="text-lg font-semibold">Rp. {price}</span>
+            <span className="text-base font-semibold">
+               Rp.{" "}
+               {price.toLocaleString("id-ID", {
+                  styles: "currency",
+                  currency: "IDR",
+               })}
+            </span>
             <span className="text-xs text-gray-400">Terjual {sold}</span>
          </div>
       </div>
    );
 };
 
-const CardFooter = () => {
+const CardFooter = (props) => {
+   const { handleAddToCart, id } = props;
+
    return (
       <div className="px-3 pb-4">
-         <Button variant="bg-emerald-500 w-full hover:bg-emerald-600">
+         <Button
+            variant="bg-emerald-500 w-full hover:bg-emerald-600"
+            onClick={() => handleAddToCart(id)}
+         >
             Add To Cart
          </Button>
       </div>
